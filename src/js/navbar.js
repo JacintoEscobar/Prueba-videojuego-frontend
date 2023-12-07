08cb4972f5df1a6bb9cde0f195e939d1cbea75a0
Jacinto Escobar Quezada <eqjo190657@upemor.edu.mx>
2023-12-07 01:56:18 -0600

    1. Se crea la página de read 2. Se agrega el template del formulario de filtrado por nombre

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,1 +1,14 @@
-$("#navbar").ready(() => $("#navbar").load("public/templates/navbar.html"));
+$("#navbar").ready(() => {
+    $("#navbar").load(
+        "public/templates/navbar.html",
+        (response, status, xhr) => {
+            if (status === "success") {
+                if (window.location.pathname === "/read.html") {
+                    $("div#search-form").load(
+                        "public/templates/read/form-search.html"
+                    );
+                }
+            }
+        }
+    );
+});
